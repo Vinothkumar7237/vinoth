ff3c0364df1682dbec380cf5a1d398471428d02c
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-01-31 16:10:45 +0530

    vinot.c

---
+++ vinot.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the number to check; ");
+scanf("%d",&num);
+if (num>0)
+ {
+  printf("The number is positive");
+  }
+ else if (num<o)
+   {
+   printf("The number is negative");
+   }
+  else
+  {
+  printf("The number is ZERO");
+  }
+  getch();
+  return 0;
+  }

46df873eb3d93ab530299401f8ab2d22e0aa370e
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-01-31 16:22:07 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the number to check:  ");
+scanf("%d",&num);
+if (num%2=o)
+  {
+   printf("The number is even");
+   }
+else if (num%2=1)
+   {
+   printf("The number is odd0");
+   }
+else
+{
+printf("The number is ZERO");
+}
+getch();
+return 0;
+}

4a99de98dacff9b986184841721d5d460b1667b7
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-02-02 13:30:23 +0530

    vowels22.c

---
+++ vowels22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the character");
+scanf("%c",&a);
+if (a=a ||a=e ||a=i ||a=o ||a=u)
+{
+printf("the character is vowels");
+}
+else
+{
+printf("the character is consonant");
+}
+getch();
+return 0;
+}

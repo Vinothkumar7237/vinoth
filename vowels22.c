7ef977a22bb2ee55f59c068b6df4233197f7003f
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-02-02 13:47:18 +0530

    Update vowels22.c

--- vowels22.c
+++ vowels22.c
@@ -4,8 +4,8 @@ int main()
 char b,a;
 printf("enter the character");
 scanf("%c",&b);
-  a=b
-if (a=a ||a=e ||a=i ||a=o ||a=u)
+  a=b;
+if (a=='a' ||a=='e' ||a=='i '||a=='o' ||a=='u')
 {
 printf("the character is vowels");
 }
